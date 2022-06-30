9115aaab093a52259e7ee98beea3c67b800804aa
Randrianarison <ericantonio123456@gmail.com>
2022-06-30 16:58:55 +0200

    init

---
+++ index.php
@@ -0,0 +1,168 @@
+<!Doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Randrianarison Antonio</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="cv.css">
+    <link rel="stylesheet" href="fontawesome-free-6.1.1-web/css/all.css">
+</head>
+<body>
+<div class="wrapper container">
+    <div class="row ">
+        <div class="col-md-4 an-bg">
+            <div class="text-end an-image">
+                <img src="../uploads/modified.jpg" alt="Randrianarison Antonio" class="img-fluid"
+                     style="border-radius: 5px">
+            </div>
+            <div class="an-section-1 container">
+                <div class="mt-4">
+                    <h2 class="an-title"><i class="fa-solid fa-address-card fa-2x an-color-icon"></i> contact</h2>
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-phone an-color-icon"></i> 034 21 208 03
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-at an-color-icon"></i> ericantonio123456@ <br>gmail.com
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-location-dot an-color-icon"></i> Logt 61 cité Mandroseza Antananarivo Madagascar
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="an-title "><i class="fa-solid fa-user-graduate fa-2x an-color-icon"></i>
+                        éducation</h2>
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-feather-pointed an-color-icon"></i> 2022: Leadership :XL OMBINA
+                        </li>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> 2022: Base de Donnés et Génie Logiciel
+                            (BACC +2) => CNTMAD (Centre National de Télé Enseignement de
+                            Madagascar)
+                        </li>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> 2019: Physique => Université
+                            d'Antananarivo Ankatso
+                        </li>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> 2018: Baccalauréat Série D => Lycée ACEEM Antananarivo Madagascar</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="an-title"><i class="fa-solid fa-language  fa-2x an-color-icon"></i> Language</h2>
+                    <ul>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> Malagasy </li>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> Anglais (moyen)</li>
+                        <li><i class="fa-solid fa-feather-pointed an-color-icon"></i> Francais</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="an-section-2 container">
+                <div>
+                    <h1>RANDRIANARISON Nantenaina Eric Antonio</h1>
+                    <ul>
+                        <li class="text-uppercase text-black" style="font-weight: bold">
+                            <i class="fa-solid fa-code"></i> Développeur web 
+                        </li>
+                        <li><i class="fa-solid fa-right-long"></i> 24 Ans</li>
+                        <li><i class="fa-solid fa-right-long"></i> Malagasy</li>
+                        <li style="margin-bottom: 53px;"> <i class="fa-solid fa-right-long"></i></i> Marié</li>
+                    </ul>
+                    <hr class="bg-dark text-center w-100">
+                </div>
+
+                <div>
+                    <h2 class="an-title"><i class="fa-solid fa-hammer fa-2x "></i> Compétence</h2>
+                    <div class="progress mt-2">
+                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" aria-valuenow="0"
+                             aria-valuemin="0" aria-valuemax="100" style="width: 90%">HTML
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" aria-valuenow="0"
+                             aria-valuemin="0" aria-valuemax="100" style="width: 85%">CSS
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar  " role="progressbar" aria-valuenow="0"
+                             aria-valuemin="0" aria-valuemax="100" style="width: 95%;background:blue">SCSS
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 90%"
+                             aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">Bootstrap
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar progress-bar-striped bg-primary" role="progressbar" aria-valuenow="0"
+                             aria-valuemin="0" aria-valuemax="100" style="width: 85%">PHP
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar  bg-secondary" role="progressbar" aria-valuenow="0"
+                             aria-valuemin="0" aria-valuemax="100" style="width: 75%">Symfony
+                        </div>
+                    </div>
+                    <div class="progress mt-2">
+                        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 85%"
+                             aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Wordpress
+                        </div>
+                    </div>
+                    
+                    
+
+                </div>
+                <div class="mt-4">
+                    <h2 class="an-title"><i class="fa-solid fa-user-tie fa-2x "></i> éxpérience et formation</h2>
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-right-long"></i> 2022: Orange Digital Center Madagascar
+                            <ul>
+                                <li> <i class="fa-solid fa-circle"></i> Formation Développement web</li>
+                            </ul>
+        
+                        </li>
+
+
+                        <li>
+                        <i class="fa-solid fa-right-long"></i> 2022: Web and Mobile Success Antsakaviro
+                            <ol>
+                            <li>
+                            <i class="fa-solid fa-circle"></i> Développeur Web back-end Symfony et Wordpress (stage) 
+                            </li>
+        
+                                <li>
+                                <i class="fa-solid fa-circle fa-small"></i> Développeur Web front-end (stage)
+                                
+                            </ol>
+                        </li>
+                        
+                        <li>
+                            <i class="fa-solid fa-right-long"></i> 2018: Open Class Room (En ligne)
+                            <ul>
+                                <li><i class="fa-solid fa-circle fa-small"></i> Initiation au Développement Web front-end</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="an-title"><i class="fa-solid fa-fire fa-2x"></i> Atouts</h2>
+                    <ul>
+                        <li><i class="fa-solid fa-spinner"></i> PROGRESSIF</li>
+                        <li><i class="fa-solid fa-handshake-simple"></i> SOCIABLE</li>
+                        <li> <i class="fa-solid fa-plus-circle"></i> CREATIVE</li>
+                        <li> <i class="fa-solid fa-book-bible"></i> CHRETIEN</li>
+                        <li> <i class="fa-brands fa-sass"></i> Scss</li>
+                        <li> <i class="fa-brands fa-git-alt"></i> GitLab</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script src="../bootstrap/jquery-3.6.0.min.js"></script>
+<script src="../bootstrap/js/bootstrap.js"></script>
+<script src="fontawesome-free-6.1.1-web/js/"></script>
+</html>
